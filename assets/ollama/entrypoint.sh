3f32ab3bdb7f7d3c5b497d73e0cfc4cf0b4b8740
Jason Han <jason.han@jpl.nasa.gov>
2025-08-17 09:47:15 -0700

    Move around some environment variables

--- assets/ollama/entrypoint.sh
+++ assets/ollama/entrypoint.sh
@@ -8,8 +8,8 @@ pid=$!
 # Pause for Ollama to start.
 sleep 5
 
-echo "Retrieve unsloth Gemma3 GGUF model..."
-/bin/ollama pull hf.co/unsloth/gemma-3-4b-it-GGUF:Q4_K_M
+echo "Retrieve default model: ${DEFAULT_MODEL}..."
+/bin/ollama pull ${DEFAULT_MODEL}
 echo "Done!"
 
 # Wait for Ollama process to finish.
